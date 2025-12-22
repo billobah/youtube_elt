ba63ce5da9628fef045d72e96f6fbc3b7291e503
Boubacar Billo Bah <billobah@yahoo.fr>
2025-12-22 22:00:09 +0100

    add function to save data as json file

--- video_stats.py
+++ video_stats.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from datetime import date
 
 import sys
 import os
@@ -14,7 +15,9 @@ API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
 maxResults = 50
 
+
 # Get Playlist ID
+
 def get_playlist_id():
     
     try:
@@ -41,7 +44,8 @@ def get_playlist_id():
         raise e
 
 
-# Get Video IDs 
+# Get Video IDs
+
 def get_video_ids(playlistId):
     
     video_ids = []
@@ -81,6 +85,7 @@ def get_video_ids(playlistId):
 
 
 # Extract Video Data
+
 def extract_video_data(video_ids):
 
     extracted_data = []
@@ -125,7 +130,17 @@ def extract_video_data(video_ids):
         raise e
 
 
+# Save data to Json file
+
+def save_to_json(extracted_data):
+    file_path = f"./data/youtube_data_{date.today()}.json"
+    
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
+    
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    extract_video_data(video_ids)
+    video_data =extract_video_data(video_ids)
+    save_to_json(video_data) 
